c87893f1d4965f4792f9636b8b0cdcde9d291830
jimmyroot <jimmybnewman@gmail.com>
2023-11-13 00:24:48 +0000

    Now time for comments

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const password1El = document.getElementById("password-1-el")
 const password2El = document.getElementById("password-2-el")
 
 let pwLength = 15
-
 let upper = true
 let number = true
 let special = true
@@ -23,27 +22,28 @@ let availableCharTypes = []
 let pw1 = null
 let pw2 = null
 
-/* Create a pool of available characters depending on the users selections.
-Lower case is the minimum */
+// Create a pool of available characters depending on the users selections.
+// Lower case is the minimum 
 function setAvailableTypes() {
     let newTypes = [charsLow]
     if (checkUpp.checked) newTypes.push(charsUpp)
     if (checkNum.checked) newTypes.push(charsNum)
     if (checkSpec.checked) newTypes.push(charsSpec)
-    availableCharTypes = newTypes
+    availableCharTypes = newTypes // Not sure this is the best way to do this? Am I copying the array or just the reference/pointer?
 }
 
 // Called on change of input-length element
 function setPasswordLength() {
     // If user leaves the field empty and tabs out we can re-populate with current length
     if (inputLength.value === "") inputLength.value = pwLength;
-    // Check validity (I think? Google said so!) set length if valid, if invalid fix it
+    // Check validity (I think? At least google said so!) set length if valid, if invalid fix it
     (inputLength.checkValidity()) ? pwLength = inputLength.value : fixInputLength()
 }
 
 // Function to fix the input length if it exceeded max or min (e.g. if user typed it manually)
 function fixInputLength() {
     let length = inputLength.value
+
     if (length > 20) {
         inputLength.value = 20
     } else if (length < 8) {
@@ -55,7 +55,8 @@ function fixInputLength() {
     setPasswordLength()
 }
 
-// Generate a password based on the configured length and avaiable characters
+// Generate a password based on the configured length and avaiable characters...we don't have to do
+// much work here as we already built the arr of available types
 function generatePassword() {
     var pw = ""
 
@@ -76,21 +77,19 @@ function getNewPasswords() {
     password2El.textContent = pw2
 }
 
-// Copy the password
+// Copy the password to clipboard
 function copyPassword(el) {
-    // console.log(true)
     let pwEl = document.getElementById(el.id)
     let text = pwEl.textContent
     navigator.clipboard.writeText(text)
+
+    // Let the user know we copied successfully, wait 1.5 secs and revert to pw
     pwEl.textContent = "Copied to clipboard..."
 
     timer = setInterval(function() {
         pwEl.textContent = text
         clearInterval(timer)
     }, 1500)
-
-    // pwToBeCopied = pwEl.textContent
-    // navigator.clipboard.writeText(pwToBeCopied)
 }
 
 setAvailableTypes()
